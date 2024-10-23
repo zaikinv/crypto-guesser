c41d405e6a568ec24126dd7bdc24a07cc0a400c7
VLADIMIR ZAIKIN <vladimir.zaikin@mercedes-benz.io>
2024-10-23 15:56:45 +0200

    Bsic rate limiting

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,17 @@ import { PriceModule } from './price/price.module';
 import { UsersModule } from './users/users.module';
 import { GuessModule } from './guess/guess.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { join } from 'path';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
+
+// 1 min window
+const THROTTLER_TTL = 60000;
+// 60 requests per 1 min window
+const THROTTLER_LIMIT = 60;
 
 @Module({
   imports: [
@@ -18,10 +25,26 @@ dotenv.config();
           ServeStaticModule.forRoot({
             rootPath: join(__dirname, 'public'),
           }),
+
+          ThrottlerModule.forRoot([
+            {
+              ttl: THROTTLER_TTL,
+              limit: THROTTLER_LIMIT,
+            },
+          ]),
         ]
       : []),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    ...(process.env.NODE_ENV === 'production'
+      ? [
+          {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard,
+          },
+        ]
+      : []),
+  ],
 })
 export class AppModule {}
